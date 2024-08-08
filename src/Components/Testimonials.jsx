bdb6ede3e98ddecd0005d01bcb4cef72f84f687a
Rohan <127836623+whorohannawriya@users.noreply.github.com>
2024-08-08 15:20:10 +0530

    Update Testimonials.jsx

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -18,14 +18,14 @@ const Testimonials = () => {
           <div className="heading flex items-center sm:mt-[30px]">
             <hr className="w-[60px] h-[4px] ml-[30px] bg-SecondaryColor rounded sm:hidden" />
             <div className="title text-SecondaryColor text-[25px] pl-[20px] sm:pl-[5px] sm:text-[20px]">
-              Front End Software Developer, Coltri Marthub Pvt. Ltd. (Aug 2024-Ongoing)
+              Front End Software Developer, Coltri Marthub Pvt. Ltd. <br/> (Aug 2024-Ongoing)
             </div>
           </div>
           <div className="feedbacks  ml-[110px] text-PrimiaryColor text-[18px] josefin-sans-light p-[20px] sm:ml-[0px]">
             <div className="desp ">
-            • Currently Working as an Intern at Coltri Marthub Pvt. Ltd. with the Software Developers team. 
-• Technical Skills: HTML, CSS, JavaScript, BootStrap, React.
-• Soft Skills: Teamwork, Time Management, Communication, Presentation skills.
+            • Currently Working as an Intern at Coltri Marthub Pvt. Ltd. with the Software Developers team.<br/>
+            • Technical Skills: HTML, CSS, JavaScript, BootStrap, React.<br/>
+            • Soft Skills: Teamwork, Time Management, Communication, Presentation skills.
 
             </div>
           
